7780c54aa701e24d35b14b19947000c52edbaaa2
ithar.malik <ithar.malik@spika.com>
2019-07-15 11:07:39 +0100

    Minor styling for user profile.

--- src/components/UsersList/index.js
+++ src/components/UsersList/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import '../../resources/css/bootstrap.css';
+import './user.css';
 
 const UsersList = ({users, loadMore}) => (
-    <div>
+    <div className="userList">
         {users.map(user => (
             <User user={user} key={user.id.value}/>
         ))}
@@ -12,10 +13,24 @@ const UsersList = ({users, loadMore}) => (
     </div>
 );
 
-const User = ({user}) => (
-    <div style={{color: 'red'}} >
-        <span>{user.name.first} {user.name.last}</span>
-    </div>
-);
+const User = ({user}) => {
+    let thumbnail = user.picture.thumbnail;
+    let firstName = upperFirst(user.name.first);
+    let lastName = upperFirst(user.name.last);
+    let address = upperFirst(user.location.city) + ' ' + upperFirst(user.location.state) + ' ' + user.nat;
+    let email = user.email;
+
+    return (
+    <div>
+        <img src={thumbnail} alt={firstName}/>
+            <h1 className="username">{firstName} {lastName}</h1>
+            <p className="address">{address}</p>
+            <p className="email">{email}</p>
+    </div>);
+};
+
+let upperFirst = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 export default UsersList;
